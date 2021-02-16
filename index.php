17f52dc99617c73ec74b24d8ca37c0e9075f5347
nasfernane <nasfernane@gmail.com>
2021-02-16 11:53:28 +0100

    maj

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+
+
+
+// $myObject = [2, 5];
+
+
+// foreach ($myObject as $value) {
+//     var_dump($value * 2);
+// };
+
+$value = "2";
+
+if ($value === 2) {
+    echo "oui";
+} else {
+    echo "non";
+}
+
+
+?>
